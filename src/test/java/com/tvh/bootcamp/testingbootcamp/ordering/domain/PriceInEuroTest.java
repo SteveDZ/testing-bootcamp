6012dcd888ab951b7fd4b8411542666287a01ac4
Steve De Zitter <de.zitter.steve@gmail.com>
2020-03-04 15:52:57 +0100

    Implement PriceInEuroTest

--- src/test/java/com/tvh/bootcamp/testingbootcamp/ordering/domain/PriceInEuroTest.java
+++ src/test/java/com/tvh/bootcamp/testingbootcamp/ordering/domain/PriceInEuroTest.java
@@ -1,18 +1,37 @@
 package com.tvh.bootcamp.testingbootcamp.ordering.domain;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PriceInEuroTest {
 
     @Test
     void add() {
+        PriceInEuro tenEuro = new PriceInEuro(new BigDecimal(10.00));
+
+        PriceInEuro thirtyEuro = tenEuro.add(new PriceInEuro(new BigDecimal(20.00)));
+
+        assertThat(thirtyEuro.getPrice()).isEqualTo(new BigDecimal(30.00));
     }
 
     @Test
     void subtract() {
+        PriceInEuro twentyEuro = new PriceInEuro(new BigDecimal(20.00));
+
+        PriceInEuro tenEuro = twentyEuro.subtract(new PriceInEuro(new BigDecimal(10.00)));
+
+        assertThat(tenEuro.getPrice()).isEqualTo(new BigDecimal(10.00));
     }
 
     @Test
     void multiply() {
+        PriceInEuro twentyEuro = new PriceInEuro(new BigDecimal(20.00));
+
+        PriceInEuro twoHundredEuro = twentyEuro.multiply(10);
+
+        assertThat(twoHundredEuro.getPrice()).isEqualTo(new BigDecimal(200.00));
     }
 }
