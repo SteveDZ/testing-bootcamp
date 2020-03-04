642df77b14c9991b3a142d7f143bfc5f9fa3caeb
Steve De Zitter <de.zitter.steve@gmail.com>
2020-03-04 15:51:45 +0100

    skeleton PriceInEuroTest

--- src/test/java/com/tvh/bootcamp/testingbootcamp/ordering/domain/PriceInEuroTest.java
+++ src/test/java/com/tvh/bootcamp/testingbootcamp/ordering/domain/PriceInEuroTest.java
@@ -5,18 +5,29 @@ import org.junit.jupiter.api.Test;
 class PriceInEuroTest {
 
     @Test
-    void getPrice() {
-    }
-
-    @Test
     void add() {
+        //Arrange - Create tenEuro priceInEuro object
+
+        //Act - add twentyEuro to tenEuro. Capture the result
+
+        //Assert - assert that the result is thirty euro
     }
 
     @Test
     void subtract() {
+        //Arrange - Create twentyEuro priceInEuro object
+
+        //Act - subtract tenEuro. Capture the result
+
+        //Assert - assert that the result is ten euro
     }
 
     @Test
     void multiply() {
+        //Arrange - Create twentyEuro priceInEuro object
+
+        //Act - multiply by 10. Capture the result
+
+        //Assert - assert that result is two hundred euro
     }
 }
