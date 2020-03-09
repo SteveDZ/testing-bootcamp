e41b00450feedc421ed4e66c444165f0ca95bbbe
Steve De Zitter <de.zitter.steve@gmail.com>
2020-03-09 20:50:21 +0100

    implemented OrderMother

--- src/test/java/com/tvh/bootcamp/testingbootcamp/ordering/domain/OrderMother.java
+++ src/test/java/com/tvh/bootcamp/testingbootcamp/ordering/domain/OrderMother.java
@@ -1,5 +1,10 @@
 package com.tvh.bootcamp.testingbootcamp.ordering.domain;
 
+import java.util.List;
+
+import static com.tvh.bootcamp.testingbootcamp.ordering.domain.Order.newOrderWithLines;
+import static java.util.Collections.singletonList;
+
 public final class OrderMother {
 
     private OrderMother() {
@@ -7,7 +12,8 @@ public final class OrderMother {
 
     //Implement the anOrderWithOneLine method
     static Order anOrderWithOneLine(OrderLine orderLine) {
-        return null;
+        List<OrderLine> orderLines = singletonList(orderLine);
+        return newOrderWithLines(orderLines);
     }
 
 }
